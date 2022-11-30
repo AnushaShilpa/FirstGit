394cee18ce0d886a6e2e86ccb9ce3274db260a1e
AnushaShilpa <anushanataraj01@gmail.com>
2022-12-01 01:16:37 +0530

    second commit

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log('I am starting git');
+// var head=document.getElementById('header-title');
+// var header=document.getElementById('main-header');
+// // header.style.borderBottom ='solid 3px #000';
+ 
+var title=document.getElementById('heading');
+title.style.color='#006f00';
+title.style.textTransform='uppercase';
+
+
+
+ 
+// const addItems = document.querySelector(".title");
+// addItems.style.textTransform = "uppercase";
+// addItems.style.color = "green";
+
