a3570db8a594c780c453c3b4f3b45eb3b919e1e9
AnushaShilpa <anushanataraj01@gmail.com>
2022-12-01 02:36:12 +0530

    queryselector and queryselectorAll

--- app.js
+++ app.js
@@ -22,9 +22,26 @@ title.style.textTransform='uppercase';
 // items.style.fontWeight="bold";
 // items.style.fontSize="25px"
 
-const li=document.getElementsByTagName('li');
-li[2].style.backgroundColor="#00ff00"
-li[1].style.fontWeight="bold";
-li.style.fontSize="25px"
+// const li=document.getElementsByTagName('li');
+// li[2].style.backgroundColor="#00ff00"
+// li[1].style.fontWeight="bold";
+// li.style.fontSize="25px"
+
+// const item =document.querySelector('.list-group-item');
+// item.nextElementSibling.style.backgroundColor="#00ff00";
 
 
+// const thirditem =document.querySelector('.list-group-item:nth-child(3)');
+// thirditem.style.display ="None";
+
+
+const items =document.querySelectorAll('.list-group-item');
+items[1].style.backgroundColor="#00ff00";
+
+const items=document.querySelectorAll('li:nth-child(odd)');
+
+
+for(var i=0;i<items.length;i++)
+{
+  items[i].style.backgroundColor="#00ff55";
+}
