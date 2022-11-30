f68d857e7de862f01a8df81c8f2107347d07418a
AnushaShilpa <anushanataraj01@gmail.com>
2022-12-01 01:49:02 +0530

    third commit

--- app.js
+++ app.js
@@ -7,9 +7,18 @@ title.style.color='#006f00';
 title.style.textTransform='uppercase';
 
 
-
- 
 // const addItems = document.querySelector(".title");
 // addItems.style.textTransform = "uppercase";
 // addItems.style.color = "green";
+
+// const third=document.getElementById('items');
+// third.children[2].style.backgroundColor="#00ff00";
+// third.style.fontFamily='solid'
+// third.style.color='#0000ff'
+// third.style.fontSize='25px'
+
+var items=document.getElementsByClassName('list-group-item');
+items[2].style.backgroundColor="#00ff00"
+items.style.fontWeight="bold";
+items.style.fontSize="25px"
 
