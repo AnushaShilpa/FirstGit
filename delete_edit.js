ffc53bc0eef4e59d57f92e2917da5bf6fab2f748
AnushaShilpa <anushanataraj01@gmail.com>
2022-12-02 19:22:03 +0530

    filter

--- delete_edit.js
+++ delete_edit.js
@@ -1,26 +1,31 @@
 const form=document.getElementById('addForm');
 const itemList=document.getElementById ('items');
+const filter= document.getElementById('filter');
+
+ 
 
 form.addEventListener('submit',addItem);
 
 itemList.addEventListener('click',removeItem);
 
-
+filter.addEventListener('keyup', filterItems);
 
 // console.log(ul);
 function addItem(e)
 {
   e.preventDefault();
   const newitem=document.getElementById('item').value;
+  const newitem1=document.getElementById('item1').value;
   const li=document.createElement('li');
   li.className="list-group-item";
   li.appendChild(document.createTextNode(newitem));
+  li.appendChild(document.createTextNode( newitem1));
   itemList.appendChild(li);
 
   const deletbtn=document.createElement('button');
   const editbtn=document.createElement("button");
   editbtn.className='btn btn-primary btn-sm float-right';
-   editbtn.appendChild(document.createTextNode('e'));
+   editbtn.appendChild(document.createTextNode('edit'));
   deletbtn.className="btn btn-danger btn-sm float-right delete";
   deletbtn.appendChild(document.createTextNode('X'));
   li.insertAdjacentElement("beforeend",deletbtn);
@@ -28,6 +33,7 @@ function addItem(e)
 
 }
 
+///removeitem
 function removeItem(e)
 {
    if(e.target.classList.contains('delete'))
@@ -42,26 +48,33 @@ function removeItem(e)
 
   
 }
-// const ul=document.querySelector("#items");
-// const li=ul.querySelectorAll(".list-group-item");
-// const editbtn=document.createElement("button");
-// editbtn.className='btn btn-primary btn-sm float-right';
-// editbtn.appendChild(document.createTextNode('e'));
-// // li.append(editbtn);
-// for(let i=0;i<li.length;i++)
-// {
-//   li[i].appendChild(editbtn);
 
-// }
+/// filter
+
+function filterItems(e){
+   // convert text to lowercase
+   const text = e.target.value.toLowerCase();
+   
+
+   // Get lis
+   const items = itemList.querySelectorAll('li');
+   // Convert to an array
+   Array.from(items).forEach(function(item){
+      const itemName = item.firstChild.textContent;
+      const description=item.children[1].textContent;
+
+     if(itemName.toLowerCase().indexOf(text) != -1 || description.toLowerCase().indexOf(text) != -1 ) {
+       item.style.display = 'block';
+     } else {
+       item.style.display = 'none';
+     }
+   });
+ }
 
 
 
-// const li=document.querySelector(".list-group-item");
-// const editbtn=document.createElement("button");
-// editbtn.className='btn btn-primary btn-sm float-right';
-// editbtn.appendChild(document.createTextNode('e'));
-// li.insertAdjacentElement("beforeend",editbtn);
-// itemList.appendChild(li);
+
+
 
 
 
