e46048423fd7a93decfc270917ec165adda8228b
AnushaShilpa <anushanataraj01@gmail.com>
2022-12-03 02:00:31 +0530

    second commit_local_storage to stringfy

--- booking.js
+++ booking.js
@@ -8,9 +8,20 @@ function addItem(e)
   const fname=document.getElementById('fname').value;
   const Email=document.getElementById('Email').value;
   const PHNO=document.getElementById('PHNO').value;
-  localStorage.setItem('fname',fname);
-  localStorage.setItem('Email',Email);
-  localStorage.setItem('PHNO',PHNO);
+  // localStorage.setItem('fname',fname);
+  // localStorage.setItem('Email',Email);
+  // localStorage.setItem('PHNO',PHNO);
 
+
+  const details=
+  {
+    fname:fname,
+    Email:Email,
+    PHNO:PHNO
+
+  }
+
+  const my_obj=JSON.stringify(details);
+  localStorage.setItem('details',my_obj)
   
 }
